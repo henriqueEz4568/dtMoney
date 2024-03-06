96854139531ae02c3905d61400bd3e9a0f4ae5d8
henriqueEz4568 <rick.scioli@gmail.com>
2024-03-06 15:57:53 -0300

    header quase feito

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { ThemeProvider } from 'styled-components'
-
+import { Transactions } from './pages/Transactions'
+import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   
 
   return (
-  <ThemeProvider>
-    <p>sdds</p>
+  <ThemeProvider theme={defaultTheme}>
+    <Transactions/>
   </ThemeProvider>
   )
 }

7882e072ce4b2e14eef5ce517dcf0ddc261b3304
henriqueEz4568 <rick.scioli@gmail.com>
2024-03-06 11:18:40 -0300

    iniciando

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { ThemeProvider } from 'styled-components'
+
+
+function App() {
+  
+
+  return (
+  <ThemeProvider>
+    <p>sdds</p>
+  </ThemeProvider>
+  )
+}
+
+export default App
